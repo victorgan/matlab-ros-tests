86a1e1a37e74dafa3911a80447a90da806d18e58
Victor Gan <victorgan@gmail.com>
2015-08-25 14:18:16 -0700

    relative to ground map, or relative to odom

--- subscribemessage.m
+++ subscribemessage.m
@@ -1,17 +1,49 @@
+close all;
 % subscribe and display
 timeOut = 3; % seconds
 
 % subscribe to point cloud, get rotation
+% points are respect to camera_depth_optical_frame
 pointcloudTopic = '/camera/depth/points';
 pointcloudSub = rossubscriber(pointcloudTopic);
-pointcloudMsg = receive(pointcloudSub,timeOut);
-xyz = readXYZ(pointcloudMsg);
+pointCloudMsgOptic = receive(pointcloudSub,timeOut);
+
+tftree = rostf;
+waitForTransform(tftree, 'odom', 'camera_depth_optical_frame');
+pointcloudMsgOdom = transform(tftree, 'odom', pointCloudMsgOptic);
+
+% Get OtoO : transform from odom to camera_depth_optical_frame
+tf_OdomToOptic = getTransform(tftree,'camera_depth_optical_frame', 'odom');
+trans = tf_OdomToOptic.Transform.Translation;
+T_OdomToOptic= [trans.X; trans.Y; trans.Z];
+quat = tf_OdomToOptic.Transform.Rotation;
+OtoOquat = [quat.W quat.X quat.Y quat.Z];
+R_OdomToOptic = quat2rotm(OtoOquat);
+
+xyzOptic = readXYZ(pointCloudMsgOptic);
 voxelGridSize = 0.05; % in metres
 ransacParams.floorPlaneTolerance = 0.02; % tolerance in m
 ransacParams.maxInclinationAngle = 30; % in degrees
-[pointCloudRotated, newOrigin, R, T] = processPointCloud(xyz, voxelGridSize, ransacParams);
+% points are respect to gan_ground_frame
+[pointCloudRotated, newOrigin, R_OpticToGround, T_OpticToGround] = processPointCloud(xyzOptic, voxelGridSize, ransacParams);
+
+xyzOdom = readXYZ(pointcloudMsgOdom);
+R_OdomToGround = R_OpticToGround*R_OdomToOptic;
+T_OdomToGround = T_OpticToGround+T_OdomToOptic;
+pointCloudRotated2 = R_OdomToGround*xyzOdom' + repmat(T_OdomToGround,1,size(xyzOdom',2));
+% pointCloudRotated2 == pointCloudRotated
+
+
+figure(1)
+% scatter3(pointcloudMsgOdom);
+plotPointCloud(pointCloud(pointCloudRotated2'), titleString);
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+hold on
 
 % subscribe to odom, get inital odometry.
+% points are respect to odom
 odomTopic = '/kinect_odometer/odometry';
 odomSub = rossubscriber(odomTopic);
 odomMsg = receive(odomSub,timeOut);
@@ -19,26 +51,18 @@ odomMsg = receive(odomSub,timeOut);
 posHistory = [];
 orientHistory = [];
 tic;
-
-figure (1)
-hold on
-scatter3(pointcloudMsg);
-title('Position History');
-xlabel('X');
-ylabel('Y');
-zlabel('Z');
-while toc < 40
+while toc < 5
     odomMsg = receive(odomSub,timeOut);
 
     position = odomMsg.Pose.Pose.Position;
     pos = [position.X; position.Y; position.Z];
-    posRotated = R*pos;
-    posHistory = [posHistory pos];
+    posRotated = R_OdomToGround*pos + T_OdomToGround;
+    posHistory = [posHistory posRotated];
 
-    orientation = odomMsg.Pose.Pose.Orientation;
-    orientQuat = [orientation.W orientation.X orientation.Y orientation.Z];
-    orientRotm = quat2rotm(orientQuat);
-    orientHistory = [orientHistory orientQuat'];
+    % orientation = odomMsg.Pose.Pose.Orientation;
+    % orientQuat = [orientation.W orientation.X orientation.Y orientation.Z];
+    % orientRotm = quat2rotm(orientQuat);
+    % orientHistory = [orientHistory orientQuat'];
 
     % figure(1)
     % subplot(1,2,1)
@@ -53,3 +77,4 @@ while toc < 40
     % zlabel('Z');
 
 end
+hold off

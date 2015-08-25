8822f3efa278fb9e98f10f00ab08ea20feb73c06
Victor Gan <victorgan@gmail.com>
2015-08-24 23:25:02 -0700

    publishing a message

---
+++ publishmessage.m
@@ -0,0 +1,20 @@
+
+joyCmdsPub = rospublisher('/chair_joy2',rostype.sensor_msgs_Joy);
+pause(2) % Wait to ensure publisher is setup
+
+while true
+    % create an empty ros message that's the type of joyCmdsPub
+    joyCmdsMsg = rosmessage(joyCmdsPub);
+
+    % roughly, linear and angular veloctiy
+    angularJoyCmd = 0.00;
+    translationalJoyCmd = 0.2;
+    joyCmdsMsg.Axes = [angularJoyCmd translationalJoyCmd];
+
+    % output time
+    joyCmdsMsg.Header.Stamp = rostime('now'); 
+
+    % send!
+    send(joyCmdsPub, joyCmdsMsg);
+    pause(2)
+end

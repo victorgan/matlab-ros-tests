7594b6c0007610db79f56c71d8abf8cd35fc7440
Victor Gan <victorgan@gmail.com>
2015-08-24 23:26:50 -0700

    topic to a variable name

--- publishmessage.m
+++ publishmessage.m
@@ -1,11 +1,13 @@
 
-joyCmdsPub = rospublisher('/chair_joy2',rostype.sensor_msgs_Joy);
+joyCmdsTopic = '/chair_joy2';
+joyCmdsPub = rospublisher(joyCmdsTopic, rostype.sensor_msgs_Joy);
 pause(2) % Wait to ensure publisher is setup
 
 while true
     % create an empty ros message that's the type of joyCmdsPub
     joyCmdsMsg = rosmessage(joyCmdsPub);
 
+    % Populate message:
     % roughly, linear and angular veloctiy
     angularJoyCmd = 0.00;
     translationalJoyCmd = 0.2;

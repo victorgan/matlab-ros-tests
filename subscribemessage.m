9ab46939746ca10ef72dd7fb73cb0c9481d585a9
Victor Gan <victorgan@gmail.com>
2015-08-25 12:33:53 -0700

    visualize orientation

--- subscribemessage.m
+++ subscribemessage.m
@@ -17,23 +17,32 @@ odomSub = rossubscriber(odomTopic);
 odomMsg = receive(odomSub,timeOut);
 
 posHistory = [];
+orientHistory = [];
 tic;
-while toc < 10
+while toc < 40
     odomMsg = receive(odomSub,timeOut);
-    showdetails(odomMsg);
 
     position = odomMsg.Pose.Pose.Position;
     pos = [position.X; position.Y; position.Z];
     posRotated = R*pos;
+    posHistory = [posHistory pos];
 
     orientation = odomMsg.Pose.Pose.Orientation;
     orientQuat = [orientation.W orientation.X orientation.Y orientation.Z];
     orientRotm = quat2rotm(orientQuat);
+    orientHistory = [orientHistory orientQuat'];
 
-    posHistory = [posHistory posRotated];
     figure(1)
+    subplot(1,2,1)
     scatter3( posHistory(1,:), posHistory(2,:), posHistory(3,:) );
     title('Position History');
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Z');
+
+    subplot(1,2,2)
+    scatter3( orientHistory(1,:), orientHistory(2,:), orientHistory(3,:) );
+    title('Orientation Quaternion History');
     xlabel('X');
     ylabel('Y');
     zlabel('Z');

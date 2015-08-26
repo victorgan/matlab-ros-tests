5fb6c9f2c625fcb0632e8a31ebabe1aedca7b258
Victor Gan <victorgan@gmail.com>
2015-08-26 13:48:36 -0700

    better stopping commands

--- copyTopic.m
+++ copyTopic.m
@@ -3,4 +3,10 @@ timerHandles.chairJoySub = rossubscriber(chairJoyTopic, rostype.sensor_msgs_Joy)
 joyCmdsTopic = '/chair_joy2';
 timerHandles.joyCmdsPub = rospublisher(joyCmdsTopic, rostype.sensor_msgs_Joy);
 timerCopyTopic = timer('TimerFcn',{@copyTopicTimer,timerHandles},'Period',0.01,'ExecutionMode','fixedSpacing');
-% timerCopyTopic.StopFcn = {@exampleHelperTurtleBotStopCallback};
+timerCopyTopic.StopFcn = {@exampleHelperTurtleBotStopCallback};
+start(timerCopyTopic)
+timerCopyTopic.Running
+
+% stop(t)
+% t.Running
+% delete(t)
